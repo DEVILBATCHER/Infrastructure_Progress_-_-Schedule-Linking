f5e4dbbad57a02e9d2090bccf17bf81c4e4fb50c
DEVILBATCHER <anwar.dev@proton.me>
2026-09-06 00:39:09 +0530

    Added the Preprrocess func for TF-IDF

--- ai_engine/ai_engine.py
+++ ai_engine/ai_engine.py
@@ -3,9 +3,12 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
+def preprocess_tfidf(text):
+    tokens = word_tokenize(text.lower())
+    stop_words = set(stopwords.words('english'))
+    filtered = [word for word in tokens if word not in stop_words]
+    lemmatized = [lemmatizer.lemmatize(word) for word in filtered]
+    return lemmatized
 text = "This is a sample text for tokenization."
-tokens = word_tokenize(text.lower())
-stop_words = set(stopwords.words('english'))
-filtered = [word for word in tokens if word not in stop_words]
-lemmatized = [lemmatizer.lemmatize(word) for word in filtered]
+lemmatized = preprocess_tfidf(text)
 print(lemmatized)
