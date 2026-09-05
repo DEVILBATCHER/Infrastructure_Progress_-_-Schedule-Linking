eb30351be27b0f0dfcbd27ce7cebb70984b19868
DEVILBATCHER <anwar.dev@proton.me>
2026-09-06 00:55:15 +0530

    Added Vectorizer for TF-IDF

--- ai_engine/ai_engine.py
+++ ai_engine/ai_engine.py
@@ -1,8 +1,17 @@
 import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
+documents = [
+    "12 inch spool erection completed at Unit 3",
+    "Install 12-inch carbon steel pipeline"
+]
+tfidf_matrix = vectorizer.fit_transform(documents)
+print(vectorizer.get_feature_names_out())
+print(tfidf_matrix.toarray())
 def preprocess_tfidf(text):
     tokens = word_tokenize(text.lower())
     stop_words = set(stopwords.words('english'))
