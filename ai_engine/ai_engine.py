5d4777b06747007e897eb775f9db8b463e036bc5
DEVILBATCHER <anwar.dev@proton.me>
2026-09-06 00:37:43 +0530

    Added Lemmatizer for TF-IDF

--- ai_engine/ai_engine.py
+++ ai_engine/ai_engine.py
@@ -1,10 +1,11 @@
 import nltk
-nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-nltk.download('stopwords')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 text = "This is a sample text for tokenization."
 tokens = word_tokenize(text.lower())
 stop_words = set(stopwords.words('english'))
 filtered = [word for word in tokens if word not in stop_words]
-print(tokens)
+lemmatized = [lemmatizer.lemmatize(word) for word in filtered]
+print(lemmatized)
