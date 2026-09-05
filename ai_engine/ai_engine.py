ef849966e5cccbf0a3514582e6da095b7a91c7a0
DEVILBATCHER <anwar.dev@proton.me>
2026-09-06 00:30:53 +0530

    Added Stopwords for TF-IDF

--- ai_engine/ai_engine.py
+++ ai_engine/ai_engine.py
@@ -1,6 +1,10 @@
 import nltk
-nltk.download('punkt')
+nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 text = "This is a sample text for tokenization."
 tokens = word_tokenize(text.lower())
+stop_words = set(stopwords.words('english'))
+filtered = [word for word in tokens if word not in stop_words]
 print(tokens)
